8a68ff126fe9672be94002f6aed5c70450c2ed22
tarak456 <tarakaratna08@gmail.com>
2026-06-10 20:06:27 +0530

    Fix static import

--- movies/urls.py
+++ movies/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.movie_list, name='movie_list'),
@@ -30,6 +32,4 @@ urlpatterns = [
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-from django.conf import settings
-from django.conf.urls.static import static
 
