4f5a0729cd3a73d40b0ce854f94cd332d2ab33c3
tarak456 <tarakaratna08@gmail.com>
2026-06-11 19:55:42 +0530

    fix: custom email backend for python 3.12 compatibility

---
+++ movies/email_backend.py
@@ -0,0 +1,26 @@
+import ssl
+import smtplib
+from django.core.mail.backends.smtp import EmailBackend
+
+class CustomEmailBackend(EmailBackend):
+    def open(self):
+        if self.connection:
+            return False
+        connection_params = {'local_hostname': self.local_hostname}
+        if self.timeout is not None:
+            connection_params['timeout'] = self.timeout
+        try:
+            self.connection = smtplib.SMTP(self.host, self.port, **connection_params)
+            if self.use_tls:
+                self.connection.ehlo()
+                context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+                context.check_hostname = False
+                context.verify_mode = ssl.CERT_NONE
+                self.connection.starttls(context=context)
+                self.connection.ehlo()
+            if self.username and self.password:
+                self.connection.login(self.username, self.password)
+            return True
+        except Exception:
+            if not self.fail_silently:
+                raise
